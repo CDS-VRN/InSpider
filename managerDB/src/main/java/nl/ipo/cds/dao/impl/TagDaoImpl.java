cec99082a84788afaee88deeaa1d7cd7cabc6b9d
annesongithub <annesongithub@users.noreply.github.com>
2015-02-02 11:53:31 +0100

    solve testFailure

--- managerDB/src/main/java/nl/ipo/cds/dao/impl/TagDaoImpl.java
+++ managerDB/src/main/java/nl/ipo/cds/dao/impl/TagDaoImpl.java
@@ -8,6 +8,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.sql.DataSource;
 
 import nl.ipo.cds.dao.TagDao;
 
@@ -26,8 +27,13 @@ public class TagDaoImpl implements TagDao {
 
 	private static final Log log = LogFactory.getLog(TagDaoImpl.class);
 
-	@Inject
+	
 	private JdbcTemplate jdbcTemplate;
+	
+	@Inject
+	public void setDataSource(DataSource dataSource) {
+		jdbcTemplate = new JdbcTemplate(dataSource);
+	}
 
 	/*
 	 * This method checks if the given tag already exists in the _tagged table from that theme.
@@ -43,11 +49,13 @@ public class TagDaoImpl implements TagDao {
 		return res.intValue() >= 1;
 	}
 
+	
+	/* This method checks if the given tag already exists in the etljob table joined with the job table.
+	 * @see nl.ipo.cds.dao.TagDao#doesTagJobWithIdExist(java.lang.String)
+	 */
 	@Override
 	public Boolean doesTagJobWithIdExist(String tag) {
 		log.debug("entered method doesTagJobWithIdExist");
-		// "SELECT fname,lname,isbn from author inner join books on author.AUTHORID = books.AUTHORID"
-
 		String sql = "select parameters from manager.etljob inner join manager.job on manager.etljob.ID = manager.job.ID where manager.job.job_type = 'TAG' and (manager.job.status = 'PREPARED' or manager.job.status = 'CREATED' or manager.job.status = 'STARTED')";
 		log.debug("query" + sql + " will be executed");
 		List<String> columnResultSet = jdbcTemplate.query(sql, new RowMapper<String>() {
@@ -55,8 +63,6 @@ public class TagDaoImpl implements TagDao {
 		          return resultSet.getString(1);
 		        }
 		      }); 
-		// String sql = "select count(parameters) from manager.etljob where parameters= ?";
-		// columnResultSet.contains(tag);
 		if (!columnResultSet.isEmpty()) {
 			for (String string : columnResultSet) {
 				if (string.isEmpty() || (!string.contains("\"tag\":\""+tag))) {
